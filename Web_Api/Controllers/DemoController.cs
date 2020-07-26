01dc523ae9a5c959103f660bdd9726443e76c3ab
uzair2299 <uzairanwar2299@gmail.com>
2020-07-27 01:42:46 +0500

    Add project files.

---
+++ Web_Api/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_Api.Controllers
+{
+    //Web API Controller is similar to ASP.NET MVC controller. It handles incoming HTTP requests and send response back to the caller
+    //Must be derived from System.Web.Http.ApiController class.
+    //Based on the incoming request URL and HTTP verb (GET/POST/PUT/PATCH/DELETE),
+    //Web API decides which Web API controller and action method to execute
+
+    //Get() method will handle HTTP GET request
+    //Post() method will handle HTTP POST request
+    //Put() mehtod will handle HTTP PUT request
+    //Delete() method will handle HTTP DELETE request
+
+
+    //Action method name can be the same as HTTP verb name or it can start with HTTP verb with any suffix(case in-sensitive)
+    //or you can apply Http verb attributes to method.
+
+
+    public class DemoController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome To Web API";
+        }
+    }
+}
---
+++ Web_Api/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Web_Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+           //We define the Web API URL format using “GlobalConfiguration” and “Configure” static method.
+           //We are passing the “Register” method from the “WebApiConfig” class as an argument into the “Configure” method.
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
